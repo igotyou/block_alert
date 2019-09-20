90ce5225009139e6728b1c0ad48f7cdbcb89d522
igotyou <aamm.dk@gmail.com>
2019-09-20 23:41:50 +0200

    Small fix
    I dun goofed. Is actually ISO 8601 now

--- recorder.lua
+++ recorder.lua
@@ -6,7 +6,7 @@ local function get_log(recorder_pos)
 end
 
 local function add_entry(recorder_pos, message)
-    message = os.date("[%Y-%d-%m %H:%M] ") .. message
+    message = os.date("[%Y-%m-%d %H:%M] ") .. message
     local log = get_log(recorder_pos)
     table.insert( log, 1,  message)
     if table.getn(log) > 500 then table.remove(log) end
