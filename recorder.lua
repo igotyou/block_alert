a7fadcd8eae68c853b798befdef6f780ee488ce0
igotyou <aamm.dk@gmail.com>
2019-09-20 23:39:47 +0200

    Changes to recorder log
    More ISO 8601 compliant

--- recorder.lua
+++ recorder.lua
@@ -6,6 +6,7 @@ local function get_log(recorder_pos)
 end
 
 local function add_entry(recorder_pos, message)
+    message = os.date("[%Y-%d-%m %H:%M] ") .. message
     local log = get_log(recorder_pos)
     table.insert( log, 1,  message)
     if table.getn(log) > 500 then table.remove(log) end
@@ -32,7 +33,7 @@ end
 function recorder.handle_block_event(pos, node_name, player_name, event_type)  
     local recorders = util.find_nodes(pos, 5, {"block_alert:recorder"})
     if recorders then
-        local message = os.date("[%d/%m %H:%M]") .. player_name .. " " .. event_type .." " .. node_name .. " at " .. minetest.pos_to_string(pos)
+        local message = player_name .. " " .. event_type .." " .. node_name .. " at " .. minetest.pos_to_string(pos)
         for _,recorder_pos in ipairs(recorders) do
             add_entry(recorder_pos, message)
         end
@@ -40,6 +41,6 @@ function recorder.handle_block_event(pos, node_name, player_name, event_type)
 end
 
 function recorder.handle_player_event(player, recorder_pos, event_type)
-    local messageString = os.date("[%d/%m %H:%M]") .. player:get_player_name() .. " " .. event_type
+    local messageString = player:get_player_name() .. " " .. event_type
     add_entry(recorder_pos, messageString)
 end
